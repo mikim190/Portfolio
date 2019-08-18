619a6b3d9e2935e48a54be2e78978fe847910f84
Kim T <kim200@kims-mbp.attlocal.net>
2019-08-18 11:34:47 -0700

    Create effect for Menu button

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,37 +1,59 @@
 import React, { Component } from "react";
+import { throws } from "assert";
 
 class Menu extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({
+      showMenu: !this.state.showMenu
+    });
+  }
+
   render() {
+    let menu = this.state.showMenu;
+    let show = menu ? "show" : "";
+
     return (
       <header>
-        <div className="menu-btn">
+        <div
+          className={`menu-btn ${menu ? "close" : ""}`}
+          onClick={this.toggleMenu}
+        >
           <div className="btn-line" />
           <div className="btn-line" />
           <div className="btn-line" />
         </div>
 
-        <nav className="menu">
-          <div className="menu-branding">
+        <nav className={`menu ${show}`}>
+          <div className={`menu-branding ${show}`}>
             <div className="portrait" />
           </div>
 
-          <ul className="menu-nav">
-            <li className="nav-item">
+          <ul className={`menu-nav ${show}`}>
+            <li className={`nav-item ${show}`}>
               <a href="/" className="nav-link">
                 HOME
               </a>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${show}`}>
               <a href="/about.html" className="nav-link">
                 ABOUT ME
               </a>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${show}`}>
               <a href="/projects.html" className="nav-link">
-                PROJECTS
+                MY PROJECTS
               </a>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${show}`}>
               <a href="/contact.html" className="nav-link">
                 CONTACT ME
               </a>
